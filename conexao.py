721c55cdf67a3c887f06dbf4a1dd9bdc3080c910
Pedro Rocha <61380674+Pedro-VRocha1@users.noreply.github.com>
2025-06-16 09:31:43 -0300

    Update conexao.py

--- conexao.py
+++ conexao.py
@@ -1,1 +1,17 @@
+import google.auth
+import pandas_gbq
+import pandas as pd
 
+def gcp_conection():    
+    credentials, project = google.auth.default(scopes=['https://www.googleapis.com/auth/cloud-platform'])
+    sql = """
+        select 1
+    """
+    df = pandas_gbq.read_gbq(sql, project_id="nome_projeto", credentials=credentials)
+    return df
+
+df = pd.DataFrame(gcp_conection())
+
+print(df)
+# print('Convertendo pra Excel')
+# df.to_excel('camiho_destino', index=False)
